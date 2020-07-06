9095f1f5b7f8c9afddc6a0395383c580bfc9ad84
unknown <sdohertyp@gmail.com>
2020-07-06 10:23:29 -0400

    first commit

---
+++ src/data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'SeanDoh',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'SeanDoh',
+  url: 'https://portfolio.smakosh.com',
+  legalName: 'John Doe',
+  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    twitter: '',
+    github: 'https://github.com/seandoh',
+    linkedin: '',
+    instagram: '',
+    youtube: '',
+    google: '',
+  },
+  googleAnalyticsID: 'number',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: 'user',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2019',
+};
